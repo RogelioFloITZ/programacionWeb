d84adf1abe7fead2fb3be7b90723d36425baaba5
Asuka095 <pizzas9999@gmail.com>
2021-12-15 00:50:39 -0600

    programacion web

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dash', function () {
+//     return view('dash.index');
+// })->name('dash');
+
+Route::get('/dash', 'App\Http\Controllers\DashboardController@index')->middleware('auth');
+
+Route::get('/dash/crud', function () {
+    return view('crud.index');
+});
+
+Route::get('/dash/crud/create', function () {
+    return view('crud.create');
+});
+
+Route::resource('articulos', 'App\Http\Controllers\ArticuloController')->names('articulos')->middleware('auth');
